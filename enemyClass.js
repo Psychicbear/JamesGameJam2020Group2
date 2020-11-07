71e710e52b6313a63bb7c22982c20b56e085abbf
Riley <rjscho@live.com.au>
2020-11-07 15:59:54 +1000

    Completed basic enemy movement

--- enemyClass.js
+++ enemyClass.js
@@ -1,44 +1,57 @@
-let enemyTypes
+let enemyTypes;
 
 class Enemy {
-    constructor(enemyNum) {
-        this.sprite = createSprite(1280,620,25,25);
-        this.sprite = Object.assign(this.sprite, enemyTypes[enemyNum])
-    }
+  constructor(enemyNum) {
+    this.sprite = createSprite(1280, 680, 25, 25);
+    Object.assign(this, enemyTypes[enemyNum]);
+    this.sprite.addImage(testSprite)
+    this.nextPoint = createVector(1280,680)
+    this.goal = createVector(20,50)
+    this.pathIndex = 0
+    this.sprite.scale = 2
+    this.sprite.rotateToDirection = true
+    this.path = pathfinding.findPath(this.nextPoint.x, this.nextPoint.y, this.goal.x, this.goal.y)
+    liveEnemies.push(this)
+  }
 
-    enemyMovement(){
-        if(!this.sprite.removed){
-            if (Math.abs(this.sprite.position.x - nextPoint.x) + Math.abs(this.sprite.position.y - nextPoint.y) < 10) {
-                
-                pathIndex += 1;
-                
-                if (pathIndex == path.length) { //this means we have reached the end
-                    //generate a new random goal for our knight to get to.
-                    //goal.x = Math.random() * 800;
-                    //goal.y = Math.random() * 600;
-                    //
-                    ////calculate path to new goal
-                    //path = pathfinding.findPath(this.sprite.position.x, this.sprite.position.y, goal.x, goal.y);
-                    //pathIndex = 0;
-                    //
-                    //nextPoint.x = this.sprite.position.x;
-                    //nextPoint.y = this.sprite.position.y;
-                    this.sprite.remove()
-                    
-                } else {
-                    //next point is first index of array
-                    nextPoint = path[pathIndex];
-                    
-                    this.sprite.velocity.x = (nextPoint.x - this.sprite.position.x) / 5;
-                    this.sprite.velocity.y = (nextPoint.y - this.sprite.position.y) / 5;
-                    
-                    if (this.sprite.velocity.x > 0) {
-                        this.sprite.mirrorX(1);
-                    } else {
-                        this.sprite.mirrorX(-1);
-                    }
-                }
-            }
+  enemyMovement() {
+    if (!this.sprite.removed) {
+      if (
+        Math.abs(this.sprite.position.x - this.nextPoint.x) +
+          Math.abs(this.sprite.position.y - this.nextPoint.y) <
+        10
+      ) {
+        this.pathIndex += 1;
+
+        if (this.pathIndex == this.path.length) {
+          //this means we have reached the end
+          //generate a new random goal for our knight to get to.
+          //goal.x = Math.random() * 800;
+          //goal.y = Math.random() * 600;
+          //
+          ////calculate path to new goal
+          //path = pathfinding.findPath(this.sprite.position.x, this.sprite.position.y, goal.x, goal.y);
+          //pathIndex = 0;
+          //
+          //nextPoint.x = this.sprite.position.x;
+          //nextPoint.y = this.sprite.position.y;
+          this.sprite.remove();
+          this.liveIndex = liveEnemies.indexOf(this)
+          liveEnemies.splice(this.liveIndex,1)
+        } else {
+          //next point is first index of array
+          this.nextPoint = this.path[this.pathIndex];
+
+          this.sprite.velocity.x = (this.nextPoint.x - this.sprite.position.x) / 5;
+          this.sprite.velocity.y = (this.nextPoint.y - this.sprite.position.y) / 5;
+
+          if (this.sprite.velocity.x > 0) {
+            this.sprite.mirrorX(1);
+          } else {
+            this.sprite.mirrorX(-1);
+          }
         }
+      }
     }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-let currentScene
-let LOADING = 0
-let MAIN_MENU = 1
-let PLAY = 2
-let LEADEROARD = 3
-let INTRUCTIONS = 4
-const W = 1280
-const H = 720
-let quickSwitch = false
-let pathFile
-let pathGrid = []
-let nextPoint = {x: 1280, y: 620};
-let goal = {x: 20, y: 50};
+let currentScene;
+let LOADING = 0;
+let MAIN_MENU = 1;
+let PLAY = 2;
+let LEADEROARD = 3;
+let INTRUCTIONS = 4;
+const W = 1280;
+const H = 720;
+let quickSwitch = false;
+let pathFile;
+let pathGrid = [];
+let nextPoint = { x: 1280, y: 680 };
+let goal = { x: 20, y: 50 };
 let pathIndex = 0;
+let liveEnemies = [];
 
 function preload() {
   //towerImg = [loadImage("assets/tower1"), loadImage("assets/tower2")]
@@ -19,24 +20,24 @@ function preload() {
   //bulletImg = [loadImage("assets/bullet1"), loadImage("assets/bullet2")]
   //mapFile =  loadStrings("map.txt")
   //baseImg = loadImage("assets/base.png")
-  gameData = loadJSON("data.JSON")
-  pathFile = loadStrings("track.txt", splitStrings)
+  gameData = loadJSON("data.JSON");
+  pathFile = loadStrings("track.txt", splitStrings);
+  testSprite = loadImage("Sprites/health.png")
 }
 
 function setup() {
-  createCanvas(W,H)
-  currentScene = 2
-  enemyTypes = gameData.enemies
-  splitStrings()
+  createCanvas(W, H);
+  currentScene = 2;
+  enemyTypes = gameData.enemies;
   pathfinding = new Pathfinding();
   pathfinding.loadGrid(pathGrid, 25, 25, 1280, 720, false);
-  path = pathfinding.findPath(nextPoint.x, nextPoint.y, goal.x, goal.y)
-  console.log(enemyTypes)
+  //path = pathfinding.findPath(nextPoint.x, nextPoint.y, goal.x, goal.y);
+  console.log(enemyTypes);
 }
 
 function draw() {
-  background(170)
-  drawSprites()
+  background(170);
+  drawSprites();
   switch (currentScene) {
     case LOADING:
       drawLoadingScreen();
@@ -56,69 +57,60 @@ function draw() {
   }
 }
 
-function drawLoadingScreen(){
+function drawLoadingScreen() {}
 
-}
+function drawMainMenuScreen() {}
 
-function drawMainMenuScreen(){
-
-}
-
-function drawPlayScreen(){
-  if(!quickSwitch){
-    enemy = new Enemy(1)
-    quickSwitch = true
-    console.log(enemy)
+function drawPlayScreen() {
+  if (!quickSwitch) {
+    enemy = new Enemy(1);
+    quickSwitch = true;
+    console.log(enemy);
   }
-  enemy.enemyMovement()
-  drawGrid(pathfinding, false);
-	drawPath(path);
-	
-	fill(0,255,0);
-	circle(nextPoint.x, nextPoint.y, 10);
-	fill(255,255,0);
-	circle(goal.x, goal.y, 20);
+  //All enemies that are alive will move along a set path
+  liveEnemies.forEach(function (enemy) {
+    enemy.enemyMovement();
+  });
+  //enemy.enemyMovement()
+  //drawGrid(pathfinding, false);
+  //drawPath(liveEnemies[0].path);
 }
 
-function drawLeaderboardScreen(){
+function drawLeaderboardScreen() {}
 
-}
+function drawInstructionsScreen() {}
 
-function drawInstructionsScreen(){
-  
-}
-
-function splitStrings(){
-  pathFile.forEach(function(line){
-    newLine = line.split(" ")
-    pathGrid.push(newLine)
-  })
+function splitStrings() {
+  pathFile.forEach(function (line) {
+    newLine = line.split(" ");
+    pathGrid.push(newLine);
+  });
 }
 
 function drawGrid(grid, showConnections) {
-	fill(255,255,255);
-	stroke(0,0,0);
-	
-	let nodes = grid.nodes;
-	for (let i = 0; i < nodes.length; i++) {
-		circle(nodes[i].x, nodes[i].y, 15);
-		if (showConnections) {
-			let cons = nodes[i].connections;
-			for (let j = 0; j < cons.length; j++) {
-				line(nodes[i].x, nodes[i].y, cons[j].x, cons[j].y);
-			}
-		}
-	}
+  fill(255, 255, 255);
+  stroke(0, 0, 0);
+
+  let nodes = grid.nodes;
+  for (let i = 0; i < nodes.length; i++) {
+    circle(nodes[i].x, nodes[i].y, 15);
+    if (showConnections) {
+      let cons = nodes[i].connections;
+      for (let j = 0; j < cons.length; j++) {
+        line(nodes[i].x, nodes[i].y, cons[j].x, cons[j].y);
+      }
+    }
+  }
 }
 
 function drawPath(path) {
-	fill(255,0,0);
-	stroke(255,0,0);
-	
-	for (let i = 0; i < path.length; i++) {
-		circle(path[i].x, path[i].y, 10);
-		if (i > 0) {
-			line(path[i].x, path[i].y, path[i-1].x, path[i-1].y);
-		}
-	}
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+
+  for (let i = 0; i < path.length; i++) {
+    circle(path[i].x, path[i].y, 10);
+    if (i > 0) {
+      line(path[i].x, path[i].y, path[i - 1].x, path[i - 1].y);
+    }
+  }
 }
