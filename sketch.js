4899cc6685b2f123f15ab7504ba727ad34b7adcd
Riley <rjscho@live.com.au>
2020-11-15 04:32:24 +1000

    tama update

--- sketch.js
+++ sketch.js
@@ -16,26 +16,28 @@ let activeElement = {};
 let waveOverride = false;
 let instructPage = 0;
 let creditsPos = H;
-let menuInit = false
 
 function preload() {
   mainScreenSong = loadSound("otherassets/mainscreensong.mp3");
   playScreenSong = loadSound("otherassets/playscreensong.mp3");
-  meowSound = loadSound("otherassets/meowsound.wav");
+  crunch = loadSound("otherassets/crunch.wav");
   shopclick = loadSound("otherassets/shopclick.wav");
   menuselect = loadSound("otherassets/menuselect.wav");
   clicksound = loadSound("otherassets/clicksound.wav");
+  hiss = loadSound("otherassets/hiss.wav");
   instructiondoc = loadStrings("instructions.txt");
   creditsdoc = loadStrings("credits.txt");
   newFont = loadFont("Minecraft.ttf");
   pauseImg = loadImage("otherassets/pause.png");
   yarnballImg = loadImage("Sprites/yarnball.png.png");
+  waterTowerIcon = loadImage("Sprites/watertowericon.png");
+  biscuitFactoryIcon = loadImage("otherassets/biccyFactory.png");
   towerImg = [
     loadImage("Sprites/tower1.png"),
     loadImage("Sprites/watertower.png"),
     loadImage("Sprites/tower3.png"),
   ];
-  bulletImg = [loadImage("Sprites/bullet1.png"),loadImage("Sprites/bullet1.png")]
+  //bulletImg = [loadImage("assets/bullet1"), loadImage("assets/bullet2")]
   enemyImg = [
     loadAnimation("Sprites/tile0001.png", "Sprites/tile0003.png"),
     loadAnimation("Sprites/whiteup0001.png", "Sprites/whiteup0003.png"),
@@ -68,7 +70,7 @@ function setup() {
   createCanvas(W, H);
   angleMode(DEGREES);
   useQuadTree(true);
-  currentScene = 1;
+  currentScene = 0;
   pathFile = convertToArray(pathFile);
   mapFile = convertToArray(mapFile);
   waveFile = convertToArray(waveFile);
@@ -109,9 +111,9 @@ function draw() {
       drawCreditsScreen();
       break;
   }
-  fill(0);
-  textSize(20);
-  text("FPS: " + round(frameRate()), 50, 50);
+  // fill(0);
+  // textSize(20);
+  // text("FPS: " + round(frameRate()), 50, 50);
 }
 
 function drawLoadingScreen(screenNumber) {
@@ -119,7 +121,7 @@ function drawLoadingScreen(screenNumber) {
   background(0);
   stroke(255, 0, 0);
   strokeWeight(10);
-  //Loading bar
+
   x = map(millis(), loadingStart, loadingStart + 5000, 0, width);
   fill(255, 0, 0);
   line(0 - width * 4, 5, x, 5);
@@ -128,8 +130,7 @@ function drawLoadingScreen(screenNumber) {
     currentScene = screenNumber;
     if (screenNumber === 1) {
       mainScreenSong.setVolume(0.05);
-      //mainScreenSong.play();
-      mainScreenSong.loop()
+      mainScreenSong.play();
     }
   }
   noStroke();
@@ -143,14 +144,11 @@ function drawMainMenuScreen() {
   textFont(newFont);
   frameRate(60);
   background(0);
-  if(!menuInit){//Setup for menu
-    yarnball = createSprite(width / 5.5, height / 3.15, 50, 50);
-    yarnball.addImage(yarnballImg);
-    yarnball2 = createSprite(width / 1.32, height / 3.15, 50, 50);
-    yarnball2.addImage(yarnballImg);
-    menuInit = true
-  }
-  
+
+  yarnball = createSprite(width / 5.5, height / 3.15, 50, 50);
+  yarnball.addImage(yarnballImg);
+  yarnball2 = createSprite(width / 1.32, height / 3.15, 50, 50);
+  yarnball2.addImage(yarnballImg);
   textSize(32);
   fill("red");
   text("CATASTROPHE: DEFENDERS OF THE YARN", width / 5, height / 3);
@@ -188,7 +186,7 @@ function drawPlayScreen() {
     pg = createGraphics(W, H);
     createMap();
     playScreenSong.setVolume(0.05);
-    playScreenSong.loop()
+    playScreenSong.play();
   }
 
   //Draws background tiles
@@ -200,19 +198,15 @@ function drawPlayScreen() {
 
   //Checks live towers for enemies in their radius
   liveTowers.forEach(function (tower) {
-    tower.selectPurchasedTower();//Triggers when selected tower is released
-   //Checks radius for enemies and fires at them
+    tower.selectPurchasedTower();
     tower.sprite.overlap(enemyGroup, function (spriteA, enemy) {
-      //Sets the tower target
       if (tower.currentTarget == 0) {
         tower.currentTarget = enemy;
       } else {
-        //Thank you to Henry for this line of code, rotates tower to enemy pos
         tower.sprite.rotation = -atan2(
           tower.sprite.position.x - tower.currentTarget.position.x,
           tower.sprite.position.y - tower.currentTarget.position.y
         );
-        //Shoots at the enemy
         if (tower.canShoot && !tower.currentTarget.removed) {
           tower.shootEnemy();
           tower.attackTimer = 0;
@@ -222,7 +216,6 @@ function drawPlayScreen() {
         }
       }
     });
-    //Sets the firerate of towers
     if (tower.attackTimer > tower.attackSpeed) {
       tower.canShoot = true;
     } else {
@@ -232,7 +225,6 @@ function drawPlayScreen() {
 
   //Checks bullet collision
   liveBullets.forEach(function (bullet) {
-    //Damages enemy and removes bullet
     bullet.sprite.overlap(enemyGroup, enemyDamage);
     if (!bullet.sprite.overlap(bullet.parent)) {
       //If bullet leaves tower radius
@@ -252,7 +244,6 @@ function drawPlayScreen() {
           towerGroup.add(selectedTower.sprite);
         }
       }
-      //Sets this tower to activeElement, showing tower info
       mouseOverSprite(tower);
     }
   });
@@ -301,7 +292,7 @@ function drawPlayScreen() {
     liveEnemies = [];
   }
 
-  //Displays game info
+  //Displays level that player is on
   textStyle(BOLD);
   fill(255);
   textSize(20);
@@ -326,7 +317,6 @@ function drawPlayScreen() {
     activeElement.sprite = inactive;
   }
   game.isInfoShown();
-  //playControls()
   // console.log(activeElement);
 }
 
@@ -336,13 +326,42 @@ function drawInstructionsScreen() {
   fill(255);
   textSize(32);
   textFont(newFont);
-  text(instructiondoc[instructPage], width / 4, height / 2.5, width / 2, height / 1.5);
+  text(
+    instructiondoc[instructPage],
+    width / 4.1,
+    height / 2.5,
+    width / 2.1,
+    height / 1.5
+  );
 
   textSize(14);
   fill("red");
   text("<Press [Esc] for Main Menu>", 10, 10, 100, 100);
   fill("green");
   text("<Press [Enter] to Play!>", 10, 80, 100, 100);
+
+  // displays images depending on what page you're on
+  if (instructPage === 7) {
+    image(biscuitFactoryIcon, width - 300, height / 2.5, 200, 200);
+  } else if (instructPage === 8) {
+    image(biscuitFactoryIcon, width - 300, height / 2.5, 200, 200);
+  } else if (instructPage === 9) {
+    image(waterTowerIcon, width - 300, height / 2.5, 200, 200);
+  } else if (instructPage === 10) {
+    image(towerImg[2], width - 300, height / 2.5, 200, 200);
+  }
+  // reverts page back to beginning if finishes and stops player from going backwards
+  if (instructPage === 13) {
+    instructPage = 0;
+  } else if (instructPage === -1) {
+    instructPage = 0;
+  }
+  // displays page number
+  fill("white");
+  text("Page Number: " + (instructPage + 1) + "/13", width - 150, height - 45, 150, 50);
+
+  fill("white");
+  text("Use Left and Right arrow keys to change pages!", 25, height - 45, 300, 50);
 }
 
 function drawCreditsScreen() {
@@ -352,6 +371,15 @@ function drawCreditsScreen() {
   textFont(newFont);
   text(creditsdoc, 150, creditsPos, width - 250, height * 10);
   creditsPos -= 1.5;
+  if (creditsPos <= -3500) {
+    currentScene = 1;
+  }
+  textFont(newFont);
+  textSize(14);
+  fill("red");
+  text("<Press [Esc] for Main Menu>", 10, 10, 100, 100);
+  fill("green");
+  text("<Press [Enter] to Play!>", 10, 80, 100, 100);
 }
 
 //Converts loaded strings to readable arrays
@@ -440,16 +468,17 @@ function targetSelect(spriteA, enemy) {
 function enemyDamage(bullet, enemy) {
   enemy.enemyHealth -= bullet.damage;
   if (enemy.enemyHealth <= 0) {
-    meowSound.play();
+    // crunch.volume(0.2);
+    // crunch.play();
     enemy.life = 1;
     game.playerMoney += enemy.enemyValue;
     liveIndex = liveEnemies.indexOf(enemy);
     liveEnemies.splice(liveIndex, 1);
   }
   if (bullet.id == 1) {
-    if(enemy.pathIndex - 10 < 0){
-      enemy.pathIndex = 0
-    } else{enemy.pathIndex -= 10;}
+    // hiss.volume(0.2);
+    // hiss.play();
+    enemy.pathIndex -= 10;
   }
   bullet.remove();
   liveIndex = liveBullets.indexOf(bullet);
